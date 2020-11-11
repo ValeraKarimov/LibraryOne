84cacf5e32c81480c9f2f2b3ed8340b5231739ca
Valerii Karimov <valeraguess@gmail.com>
2020-11-11 13:51:53 +0200

    work with css-classes and event handlers

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,18 @@
-console.log('Hello!');
+import './lib/lib';
+import $ from './lib/lib';
+
+$('div').show();
+
+$('.active').addClass('hello', 'world'); 
+
+$('.active').toggleClass('ioan');
+
+$('button').on('click', function() {
+  $(this).toggleClass('active');
+});
+
+$('.active').click(sayHello);
+
+function sayHello() {
+  console.log('Hello');
+}
