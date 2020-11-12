d2d0b30e9ec24ce8fcbfadb5d0b96d2740233641
Valerii Karimov <valeraguess@gmail.com>
2020-11-12 20:01:34 +0200

    dynamic creating modal windows

--- src/js/main.js
+++ src/js/main.js
@@ -11,3 +11,35 @@ $('[data-count="second"]').on('click', () => {
 $('button').eq(2).on('click', () => {
   $('.w-500').fadeToggle(800);
 });
+
+$('#trigger').click(() => $('#trigger').createModal({
+  text: {
+    title: 'Modal title',
+    body: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Est ipsum, corrupti optio ex illum, saepe quos voluptatum omnis deserunt libero blanditiis, porro fuga non maiores nam quibusdam distinctio eos magnam.'
+  }, btns: {
+    count: 3,
+    settings: [
+      [
+        'Close',
+        ['btn-danger', 'mr-10'],
+        true
+      ],
+      [
+        'Save changes',
+        ['btn-success'],
+        false,
+        () => {
+          alert('Дані збережені');
+        }
+      ],
+      [
+        'Another btn',
+        ['btn-warning', 'mr-10'],
+        false,
+        () => {
+          alert('hello world');
+        }
+      ]
+    ]
+  }
+}));
